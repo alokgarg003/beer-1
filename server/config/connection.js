1e3e63cebabbb90769b6f965cdee858d0fa10aa3
alokgarg003 <alokgarg003@gmail.com>
2023-11-11 00:26:41 +0530

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb:http://localhost:3001/graphql'
+  process.env.MONGODB_URI || 'mongodb://localhost:3001/graphql',
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
 module.exports = mongoose.connection;
