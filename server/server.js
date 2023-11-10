f2c923c5ad1fadfb867e62000aabbe9f7fea0787
alokgarg003 <alokgarg003@gmail.com>
2023-11-10 07:48:15 +0530

    d

--- server/server.js
+++ server/server.js
@@ -20,16 +20,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   // allows refresh on production
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client\public\index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
 
 // for local testing or not used?
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client\public\index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
