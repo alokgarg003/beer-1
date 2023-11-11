1fb5fe32bea084db9383f542a3155708d77a8f6c
alokgarg003 <alokgarg003@gmail.com>
2023-11-11 10:03:36 +0530

    x

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const cors = require("cors")
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -18,7 +19,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(cors());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   // allows refresh on production
